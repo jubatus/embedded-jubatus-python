jubatus/embedded-jubatus-python

commit 0f4e895ae0bb4729ad69c7318229eb90f5173575

Translate all c++ exception into python exception

src/classifier.pyx
@@ -1,5 +1,6 @@
-cdef class _ClassifierWrapper:
+cdef class Classifier(_JubatusBase):
     cdef _Classifier *_handle
+    cdef object classes_
 
     def __cinit__(self):
         self._handle = NULL
@@ -11,14 +12,20 @@ cdef class _ClassifierWrapper:
     def _init(self, config):
         self._handle = new _Classifier(config)
         self.classes_ = None
-        typ, ver = b'classifier', 1
-        return (
-            lambda: self._handle.get_config().decode('utf8'),
-            lambda: self._handle.dump(typ, ver),
-            lambda x: self._handle.load(x, typ, ver),
-            lambda: self._handle.clear(),
-            typ,
-        )
+        self._type, self._model_ver = b'classifier', 1
+
+    def get_config(self):
+        return self._handle.get_config().decode('utf8')
+
+    def save_bytes(self):
+        return self._handle.dump(self._type, self._model_ver)
+
+    def load_bytes(self, x):
+        return self._handle.load(x, self._type, self._model_ver)
+
+    def clear(self):
+        self._handle.clear()
+        return True
 
     def train(self, data):
         cdef string l

tests/test_classifier.py
@@ -244,3 +244,13 @@ class TestClassifier(unittest.TestCase):
             fd.write(b'{"hoge')
             fd.flush()
             self.assertRaises(ValueError, Classifier, fd.name)
+
+    def test_issue_30(self):
+        path = '/tmp/127.0.0.1_0_classifier_foo.jubatus'
+        open(path, 'wb').close()
+        try:
+            Classifier({'method': 'perceptron', 'converter': {}}).load('foo')
+        except Exception:
+            pass
+        finally:
+            os.remove(path)

src/_wrapper.pxd
@@ -13,82 +13,82 @@ ctypedef uint64_t node_id_t
 cdef extern from '_wrapper.h' nogil:
     cdef cppclass _Classifier:
         _Classifier(const string& config) except +
-        void train(const string& config, const datum& d)
-        vector[classify_result_elem] classify(const datum& d)
-        vector[pair[string, uint64_t]] get_labels()
-        bool set_label(const string& new_label)
-        bool delete_label(const string& target_label)
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        void train(const string& config, const datum& d) except +
+        vector[classify_result_elem] classify(const datum& d) except +
+        vector[pair[string, uint64_t]] get_labels() except +
+        bool set_label(const string& new_label) except +
+        bool delete_label(const string& target_label) except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Regression:
         _Regression(const string& config) except +
-        void train(float score, const datum& d)
-        float estimate(const datum& d)
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        void train(float score, const datum& d) except +
+        float estimate(const datum& d) except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Recommender:
         _Recommender(const string& config) except +
-        void clear_row(const string& id)
-        void update_row(const string& id, const datum& d)
-        datum complete_row_from_id(const string& id)
-        datum complete_row_from_datum(const datum& d)
-        vector[pair[string, float]] similar_row_from_id(const string& id, size_t ret_num)
-        vector[pair[string, float]] similar_row_from_datum(const datum& d, size_t ret_num)
-        datum decode_row(const string& id)
-        vector[string] get_all_rows()
-        float calc_similarity(const datum& l, const datum& r)
-        float calc_l2norm(const datum& d)
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        void clear_row(const string& id) except +
+        void update_row(const string& id, const datum& d) except +
+        datum complete_row_from_id(const string& id) except +
+        datum complete_row_from_datum(const datum& d) except +
+        vector[pair[string, float]] similar_row_from_id(const string& id, size_t ret_num) except +
+        vector[pair[string, float]] similar_row_from_datum(const datum& d, size_t ret_num) except +
+        datum decode_row(const string& id) except +
+        vector[string] get_all_rows() except +
+        float calc_similarity(const datum& l, const datum& r) except +
+        float calc_l2norm(const datum& d) except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _NearestNeighbor:
         _NearestNeighbor(const string& config) except +
-        void set_row(const string& id, const datum& d)
-        vector[pair[string, float]] neighbor_row_from_id(const string& id, size_t size)
-        vector[pair[string, float]] neighbor_row_from_datum(const datum& d, size_t size)
-        vector[pair[string, float]] similar_row_from_id(const string& id, size_t size)
-        vector[pair[string, float]] similar_row_from_datum(const datum& d, size_t size)
-        vector[string] get_all_rows()
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        void set_row(const string& id, const datum& d) except +
+        vector[pair[string, float]] neighbor_row_from_id(const string& id, size_t size) except +
+        vector[pair[string, float]] neighbor_row_from_datum(const datum& d, size_t size) except +
+        vector[pair[string, float]] similar_row_from_id(const string& id, size_t size) except +
+        vector[pair[string, float]] similar_row_from_datum(const datum& d, size_t size) except +
+        vector[string] get_all_rows() except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Anomaly:
         _Anomaly(const string& config) except +
-        void clear_row(const string& id)
-        pair[string, float] add(const datum& d)
-        float update(const string& id, const datum& d)
-        float overwrite(const string& id, const datum& d)
-        float calc_score(const datum& d)
-        vector[string] get_all_rows()
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        void clear_row(const string& id) except +
+        pair[string, float] add(const datum& d) except +
+        float update(const string& id, const datum& d) except +
+        float overwrite(const string& id, const datum& d) except +
+        float calc_score(const datum& d) except +
+        vector[string] get_all_rows() except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Clustering:
         _Clustering(const string& config) except +
-        void push(const vector[indexed_point]& points)
-        size_t get_revision()
-        vector[vector[pair[double, datum]]] get_core_members()
-        vector[datum] get_k_center()
-        datum get_nearest_center(const datum& d)
-        vector[pair[double, datum]] get_nearest_members(const datum& d)
-        vector[vector[pair[double, string]]] get_core_members_light()
-        vector[pair[double, string]] get_nearest_members_light(const datum& d)
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        void push(const vector[indexed_point]& points) except +
+        size_t get_revision() except +
+        vector[vector[pair[double, datum]]] get_core_members() except +
+        vector[datum] get_k_center() except +
+        datum get_nearest_center(const datum& d) except +
+        vector[pair[double, datum]] get_nearest_members(const datum& d) except +
+        vector[vector[pair[double, string]]] get_core_members_light() except +
+        vector[pair[double, string]] get_nearest_members_light(const datum& d) except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Burst:
         cppclass Batch:
@@ -96,90 +96,90 @@ cdef extern from '_wrapper.h' nogil:
             int relevant_data_count
             double burst_weight
         _Burst(const string& config) except +
-        bool add_document(const string& str, double pos)
-        pair[double,vector[Batch]] get_result(const string& keyword)
-        pair[double,vector[Batch]] get_result_at(const string& keyword, double pos)
-        map[string, pair[double,vector[Batch]]] get_all_bursted_results()
-        map[string, pair[double,vector[Batch]]] get_all_bursted_results_at(double pos)
-        vector[keyword_with_params] get_all_keywords()
-        bool add_keyword(const string& keyword, const keyword_params& params)
-        bool remove_keyword(const string& keyword)
-        bool remove_all_keywords()
-        void calculate_results()
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        bool add_document(const string& str, double pos) except +
+        pair[double,vector[Batch]] get_result(const string& keyword) except +
+        pair[double,vector[Batch]] get_result_at(const string& keyword, double pos) except +
+        map[string, pair[double,vector[Batch]]] get_all_bursted_results() except +
+        map[string, pair[double,vector[Batch]]] get_all_bursted_results_at(double pos) except +
+        vector[keyword_with_params] get_all_keywords() except +
+        bool add_keyword(const string& keyword, const keyword_params& params) except +
+        bool remove_keyword(const string& keyword) except +
+        bool remove_all_keywords() except +
+        void calculate_results() except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Bandit:
         _Bandit(const string& config) except +
-        bool register_arm(const string& arm_id)
-        bool delete_arm(const string& arm_id)
-        string select_arm(const string& player_id)
-        bool register_reward(const string& player_id, const string& arm_id, double reward)
-        map[string, arm_info] get_arm_info(const string& player_id)
-        bool reset(const string& player_id)
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        bool register_arm(const string& arm_id) except +
+        bool delete_arm(const string& arm_id) except +
+        string select_arm(const string& player_id) except +
+        bool register_reward(const string& player_id, const string& arm_id, double reward) except +
+        map[string, arm_info] get_arm_info(const string& player_id) except +
+        bool reset(const string& player_id) except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Stat:
         _Stat(const string& config) except +
-        void push(const string& key, double value)
-        double sum(const string& key)
-        double stddev(const string& key)
-        double max(const string& key)
-        double min(const string& key)
-        double entropy()
-        double moment(const string& key, int degree, double center)
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        void push(const string& key, double value) except +
+        double sum(const string& key) except +
+        double stddev(const string& key) except +
+        double max(const string& key) except +
+        double min(const string& key) except +
+        double entropy() except +
+        double moment(const string& key, int degree, double center) except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Weight:
         _Weight(const string& config) except +
-        sfv_t update(const datum& d)
-        sfv_t calc_weight(const datum& d)
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        sfv_t update(const datum& d) except +
+        sfv_t calc_weight(const datum& d) except +
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
     cdef cppclass _Graph:
         _Graph(const string& config) except +
-        string create_node()
-        bool remove_node(const string& node_id)
+        string create_node() except +
+        bool remove_node(const string& node_id) except +
         bool update_node(const string& node_id,
-                         const prop_t& properties)
+                         const prop_t& properties) except +
         edge_id_t create_edge(const string& src,
                               const string& target,
-                              const prop_t& properties)
+                              const prop_t& properties) except +
         bool update_edge(edge_id_t edge_id,
-                         const prop_t& properties)
-        void remove_edge(edge_id_t edge_id)
+                         const prop_t& properties) except +
+        void remove_edge(edge_id_t edge_id) except +
 
         double get_centrality(const string& node_id,
                               int centrality_type,
-                              const preset_query& q)
-        void add_centrality_query(const preset_query& q)
-        void add_shortest_path_query(const preset_query& q)
-        void remove_centrality_query(const preset_query& q)
-        void remove_shortest_path_query(const preset_query& q)
+                              const preset_query& q) except +
+        void add_centrality_query(const preset_query& q) except +
+        void add_shortest_path_query(const preset_query& q) except +
+        void remove_centrality_query(const preset_query& q) except +
+        void remove_shortest_path_query(const preset_query& q) except +
         vector[node_id_t] get_shortest_path(const string& src,
                                             const string& target,
                                             uint64_t max_hop,
-                                            const preset_query& q)
+                                            const preset_query& q) except +
 
-        void update_index()
-        node_info get_node(const string& node_id)
-        edge_info get_edge(edge_id_t eid)
+        void update_index() except +
+        node_info get_node(const string& node_id) except +
+        edge_info get_edge(edge_id_t eid) except +
 
-        string dump(const string& type, uint64_t ver)
-        void load(const string& data, const string& type, uint64_t ver)
-        string get_config()
-        void clear()
+        string dump(const string& type, uint64_t ver) except +
+        void load(const string& data, const string& type, uint64_t ver) except +
+        string get_config() except +
+        void clear() except +
 
 cdef extern from 'jubatus/core/fv_converter/datum.hpp' namespace 'jubatus::core::fv_converter' nogil:
     cdef cppclass datum:

src/anomaly.pyx
@@ -1,4 +1,4 @@
-cdef class _AnomalyWrapper:
+cdef class Anomaly(_JubatusBase):
     cdef _Anomaly *_handle
 
     def __cinit__(self):
@@ -10,14 +10,20 @@ cdef class _AnomalyWrapper:
 
     def _init(self, config):
         self._handle = new _Anomaly(config)
-        typ, ver = b'anomaly', 1
-        return (
-            lambda: self._handle.get_config().decode('utf8'),
-            lambda: self._handle.dump(typ, ver),
-            lambda x: self._handle.load(x, typ, ver),
-            lambda: self._handle.clear(),
-            typ
-        )
+        self._type, self._model_ver = b'anomaly', 1
+
+    def get_config(self):
+        return self._handle.get_config().decode('utf8')
+
+    def save_bytes(self):
+        return self._handle.dump(self._type, self._model_ver)
+
+    def load_bytes(self, x):
+        return self._handle.load(x, self._type, self._model_ver)
+
+    def clear(self):
+        self._handle.clear()
+        return True
 
     def clear_row(self, id_):
         self._handle.clear_row(id_.encode('utf8'))

src/bandit.pyx
@@ -1,4 +1,4 @@
-cdef class _BanditWrapper:
+cdef class Bandit(_JubatusBase):
     cdef _Bandit *_handle
 
     def __cinit__(self):
@@ -10,14 +10,20 @@ cdef class _BanditWrapper:
 
     def _init(self, config):
         self._handle = new _Bandit(config)
-        typ, ver = b'bandit', 1
-        return (
-            lambda: self._handle.get_config().decode('utf8'),
-            lambda: self._handle.dump(typ, ver),
-            lambda x: self._handle.load(x, typ, ver),
-            lambda: self._handle.clear(),
-            typ,
-        )
+        self._type, self._model_ver = b'bandit', 1
+
+    def get_config(self):
+        return self._handle.get_config().decode('utf8')
+
+    def save_bytes(self):
+        return self._handle.dump(self._type, self._model_ver)
+
+    def load_bytes(self, x):
+        return self._handle.load(x, self._type, self._model_ver)
+
+    def clear(self):
+        self._handle.clear()
+        return True
 
     def register_arm(self, arm_id):
         return self._handle.register_arm(arm_id.encode('utf8'))

src/burst.pyx
@@ -9,7 +9,7 @@ cdef _to_window(const pair[double, vector[_Burst.Batch]]& r):
 cdef _to_window_list(map[string, pair[double, vector[_Burst.Batch]]]& r):
     return {it.first.decode('utf8'): _to_window(it.second) for it in r}
 
-cdef class _BurstWrapper:
+cdef class Burst(_JubatusBase):
     cdef _Burst *_handle
 
     def __cinit__(self):
@@ -21,14 +21,20 @@ cdef class _BurstWrapper:
 
     def _init(self, config):
         self._handle = new _Burst(config)
-        typ, ver = b'burst', 1
-        return (
-            lambda: self._handle.get_config().decode('utf8'),
-            lambda: self._handle.dump(typ, ver),
-            lambda x: self._handle.load(x, typ, ver),
-            lambda: self._handle.clear(),
-            typ,
-        )
+        self._type, self._model_ver = b'burst', 1
+
+    def get_config(self):
+        return self._handle.get_config().decode('utf8')
+
+    def save_bytes(self):
+        return self._handle.dump(self._type, self._model_ver)
+
+    def load_bytes(self, x):
+        return self._handle.load(x, self._type, self._model_ver)
+
+    def clear(self):
+        self._handle.clear()
+        return True
 
     def add_documents(self, data):
         ret = 0

src/clustering.pyx
@@ -1,4 +1,4 @@
-cdef class _ClusteringWrapper:
+cdef class Clustering(_JubatusBase):
     cdef _Clustering *_handle
 
     def __cinit__(self):
@@ -10,14 +10,20 @@ cdef class _ClusteringWrapper:
 
     def _init(self, config):
         self._handle = new _Clustering(config)
-        typ, ver = b'clustering', 1
-        return (
-            lambda: self._handle.get_config().decode('utf8'),
-            lambda: self._handle.dump(typ, ver),
-            lambda x: self._handle.load(x, typ, ver),
-            lambda: self._handle.clear(),
-            typ,
-        )
+        self._type, self._model_ver = b'clustering', 1
+
+    def get_config(self):
+        return self._handle.get_config().decode('utf8')
+
+    def save_bytes(self):
+        return self._handle.dump(self._type, self._model_ver)
+
+    def load_bytes(self, x):
+        return self._handle.load(x, self._type, self._model_ver)
+
+    def clear(self):
+        self._handle.clear()
+        return True
 
     def push(self, points):
         cdef datum d

src/embedded_jubatus.pyx
@@ -58,7 +58,10 @@ from jubatus.graph.types import Query
 from jubatus.graph.types import ShortestPathQuery
 
 
-class _JubatusBase(object):
+cdef class _JubatusBase:
+    cdef object _type
+    cdef int _model_ver
+
     def __init__(self, config):
         import json
         if isinstance(config, str):
@@ -68,21 +71,15 @@ class _JubatusBase(object):
             json.loads(config)
         else:
             config = json.dumps(config, sort_keys=True, indent=4)
-        (self.get_config, self.save_bytes, self.load_bytes,
-         self._clear_impl, typ) = self._init(config.encode('utf8'))
-        if str != bytes and isinstance(typ, bytes):
-            typ = typ.decode('ascii')
-        self._type = typ
+        self._init(config.encode('utf8'))
 
     def _get_model_path(self, id_):
-        host, port = '127.0.0.1', 0
+        host, port, typ = '127.0.0.1', 0, self._type
+        if str != bytes and isinstance(typ, bytes):
+            typ = typ.decode('ascii')
         path = '/tmp/{host}_{port}_{type}_{id}.jubatus'.format(
-            host=host, port=port, type=self._type, id=id_)
+            host=host, port=port, type=typ, id=id_)
         return (path, '{host}_{port}'.format(host=host, port=port))
-
-    def clear(self):
-        self._clear_impl()
-        return True
 
     def load(self, id_):
         path, name = self._get_model_path(id_)
@@ -135,36 +132,3 @@ include 'regression.pyx'
 include 'stat.pyx'
 include 'weight.pyx'
 include 'graph.pyx'
-
-class Anomaly(_JubatusBase, _AnomalyWrapper):
-    pass
-
-class Bandit(_JubatusBase, _BanditWrapper):
-    pass
-
-class Burst(_JubatusBase, _BurstWrapper):
-    pass
-
-class Classifier(_JubatusBase, _ClassifierWrapper):
-    pass
-
-class Clustering(_JubatusBase, _ClusteringWrapper):
-    pass
-
-class NearestNeighbor(_JubatusBase, _NearestNeighborWrapper):
-    pass
-
-class Recommender(_JubatusBase, _RecommenderWrapper):
-    pass
-
-class Regression(_JubatusBase, _RegressionWrapper):
-    pass
-
-class Stat(_JubatusBase, _StatWrapper):
-    pass
-
-class Weight(_JubatusBase, _WeightWrapper):
-    pass
-
-class Graph(_JubatusBase, _GraphWrapper):
-    pass

commit 433ce7b35d65c85f247a0bca754f801e1a484ca4

Fix classes_ property accessibility in scikit-learn compatible classifier API
Regression in commit 0f4e895ae0bb4729ad69c7318229eb90f5173575

src/classifier.pyx
@@ -1,6 +1,6 @@
 cdef class Classifier(_JubatusBase):
     cdef _Classifier *_handle
-    cdef object classes_
+    cdef object _classes
 
     def __cinit__(self):
         self._handle = NULL
@@ -11,7 +11,7 @@ cdef class Classifier(_JubatusBase):
 
     def _init(self, config):
         self._handle = new _Classifier(config)
-        self.classes_ = None
+        self._classes = None
         self._type, self._model_ver = b'classifier', 1
 
     def get_config(self):
@@ -76,9 +76,13 @@ cdef class Classifier(_JubatusBase):
     def delete_label(self, target_label):
         return self._handle.delete_label(target_label.encode('utf8'))
 
+    @property
+    def classes_(self):
+        return self._classes
+
     def fit(self, X, y):
         self._handle.clear()
-        self.classes_ = None
+        self._classes = None
         return self.partial_fit(X, y)
 
     def partial_fit(self, X, y):
@@ -93,9 +97,9 @@ cdef class Classifier(_JubatusBase):
         cdef int is_csr = (type(X).__name__ == 'csr_matrix')
         if not (is_ndarray or is_csr):
             raise ValueError
-        if self.classes_ is None:
-            self.classes_ = []
-        max_label = len(self.classes_) - 1
+        if self._classes is None:
+            self._classes = []
+        max_label = len(self._classes) - 1
         for i in range(rows):
             if is_ndarray:
                 ndarray_to_datum(X, i, d, cache)
@@ -106,8 +110,8 @@ cdef class Classifier(_JubatusBase):
             if max_label < y[i]:
                 max_label = y[i]
             self._handle.train(cache[y[i]], d)
-        for j in range(len(self.classes_), max_label + 1):
-            self.classes_.append(j)
+        for j in range(len(self._classes), max_label + 1):
+            self._classes.append(j)
         return self
 
     def decision_function(self, X):

tests/test_classifier.py
@@ -175,6 +175,7 @@ class TestClassifier(unittest.TestCase):
         self.assertTrue(not isinstance(y[0], (list, tuple, np.ndarray)))
         self.assertTrue(y[0] > 0)
         self.assertTrue(y[1] < 0)
+        self.assertEqual([0, 1], sorted(x.classes_))
 
     @skipIf(not NUMPY, 'numpy cannot import')
     def test_sparse(self):